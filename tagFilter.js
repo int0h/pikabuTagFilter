b68d3472c5daf7088c41b2cada9264a8ecb076d1
int0h <int0h@yandex.ru>
2017-07-18 23:39:18 +0300

    bug fixes

--- tagFilter.js
+++ tagFilter.js
@@ -1,45 +1,45 @@
-// ==UserScript==
-// @name         tagFilter
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Фильтрация тегов с логическими выражениями
-// @author       int0h
-// @match        pikabu.ru/*
-// @grant        none
-// ==/UserScript==
+    // ==UserScript==
+    // @name         tagFilter
+    // @namespace    http://tampermonkey.net/
+    // @version      0.1
+    // @description  Фильтрация тегов с логическими выражениями
+    // @author       int0h
+    // @match        pikabu.ru/*
+    // @grant        none
+    // ==/UserScript==
 
-(function() {
-    'use strict';
+    (function() {
+        'use strict';
 
-    let filterFunc;
+        let filterFunc;
 
-    function init() {
-        let expression = localStorage.tagFilter_expression;
-        if (expression) {
-            expression = prompt('Введите выражение для фильтрации тегов в виде: tags.кот && tags["с пробелом"]');
-            expression = localStorage.tagFilter_expression;
+        function init() {
+            let expression = localStorage.tagFilter_expression;
+            if (!expression) {
+                expression = prompt('Введите выражение для фильтрации тегов в виде: tags.кот && tags["с пробелом"]');
+                localStorage.tagFilter_expression = expression;
+            }
+            filterFunc = genFilter(expression);
         }
-        filterFunc = genFilter(expression);
-    }
 
-    function genFilter(expString) {
-        return new Function('tags', 'return ' + expString);
-    }
+        function genFilter(expString) {
+            return new Function('tags', 'return ' + expString);
+        }
 
-    init();
+        init();
 
-    function main() {
-        const storiesToHide = $('.story').filter((id, story) => {
-            const tagList = $(story)
-                .find('.story__tag')
-                .toArray()
-                .map(item => item.textContent.trim());
-            let tagHash = {};
-            tagList.forEach(tag => tags[tag] = true);
-            return filterFunc(tagHash);
-        });
-        storiesToHide.hide();
-    }
+        function main() {
+            const storiesToHide = $('.story').filter((id, story) => {
+                const tagList = $(story)
+                    .find('.story__tag')
+                    .toArray()
+                    .map(item => item.textContent.trim().toLowerCase());
+                let tagHash = {};
+                tagList.forEach(tag => tagHash[tag] = true);
+                return filterFunc(tagHash);
+            });
+            storiesToHide.hide();
+        }
 
-    main();
-})();
+        main();
+    })();
